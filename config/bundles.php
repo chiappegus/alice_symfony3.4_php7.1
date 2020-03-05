cddca5534fa77457d6e1caa32fc47afb992799b7
gchiappe <gchiappe@magyp.gob.ar>
2020-03-05 15:31:15 -0300

    Muy bueno ya avanzando de a poco con el tema de las relaciones. :)

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
